e0ec192eda3f0036586372dd876c6b14f0cd9e4a
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-05-30 09:56:18 +0900

    직접 임포트

--- app/model/__init__.py
+++
@@ -1,8 +0,0 @@
-from api.domain.character.models import Character
-from api.domain.skill.models import Skill
-from api.domain.property.models import Property
-from api.domain.enemy.models import Enemy
-from api.domain.character.models import Character
-from api.domain.stage.models import StageEnemy, Stage
-from api.domain.user.models import User
-from api.domain.board.models import Post
--- app/model/base.py
+++ app/model/base.py
@@ -1,7 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from db.crud.crud import engine
-from . import *
+from api.domain.character.models import Character
+from api.domain.skill.models import Skill
+from api.domain.property.models import Property
+from api.domain.enemy.models import Enemy
+from api.domain.character.models import Character
+from api.domain.stage.models import StageEnemy, Stage
+from api.domain.user.models import User
+from api.domain.board.models import Post
 
 Base = declarative_base()
 
