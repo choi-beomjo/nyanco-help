23ea60c41efaab7a72206804904ee210024ad94a
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-05-30 09:50:01 +0900

    add import

--- app/model/base.py
+++ app/model/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from db.crud.crud import engine
+from . import *
 
 Base = declarative_base()
 
