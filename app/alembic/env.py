61896762a5fc635c6223ebcf0048fe7e4740fb4d
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-06-09 20:26:57 +0900

    env.py 수정

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -10,6 +10,11 @@ from model.base import Base
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+db_url = os.getenv("DB_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
