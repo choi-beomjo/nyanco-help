c564f6cee59bd4c300da8ac36a9860d8dd2d18df
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-06-10 22:16:34 +0900

    bugfix: 아 제발좀 (#48)

--- app/api/domain/skill/utils.py
+++ app/api/domain/skill/utils.py
@@ -6,7 +6,15 @@ from db.crud.crud import CRUD
 def get_skills_from_db(crud: CRUD, skill_info={}):
     filters = skill_info
 
-    skills = crud.read_all(Skill, custom_conditions=[filters])
+    skills = crud.read_all(Skill)
+
+    return skills
+
+
+def get_skill_from_db(crud: CRUD, skill_info={}):
+    filters = skill_info
+
+    skills = crud.read(Skill, filters=filters)
 
     return skills
 
