0d8d10fc3193aae7ba3801fdcf0557ccb583b085
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-06-10 20:12:26 +0900

    캐릭터 중복 추가 불가 로직 추가 (#48)

--- app/api/domain/character/api.py
+++ app/api/domain/character/api.py
@@ -21,6 +21,10 @@ def get_character_list(crud: CRUD = Depends(get_crud)):
 def post_character(character_info: CharacterData, crud: CRUD = Depends(get_crud), current_user=Depends(admin_required)):
     
     character_data = character_info.dict()
+    # 이미 존재하는 캐릭터 검증
+    if get_character_from_db(character_data['name'], crud=crud):
+        raise HTTPException(status_code=409, detail="Character already exists")
+    
     add_character_to_db(character_data=character_data, crud=crud)
 
     return Msg(msg="success")
