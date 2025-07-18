dbe2a0591d886603cb3f032bdd9e238d5d316057
beomjo <bbjj009911@gmail.com>
2025-07-18 14:58:23 +0900

    feat: 서비 시작 시 모델 프리 로딩 (#66)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+import pickle
+
+import joblib
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
+import torch
 from api.api import api
 
 
@@ -16,6 +19,26 @@ app.add_middleware(
 )
 
 app.include_router(api, prefix="/api")
+
+@app.on_event('startup')
+def load_model():
+    global model
+    model = torch.load("../models/deeprec_model.pt", weights_only=False)
+    model.eval()
+
+    global node_mapping
+    global scaler
+    global embeddings
+    global char_stat_dict
+
+    embeddings = torch.load("../models/full_embeddings.pt")
+    node_mapping = torch.load(("../models/node_mapping_full.pt"))
+
+    scaler = joblib.load("../models/stat_scaler.pkl")
+
+    with open("../models/char_stat_dict.pkl", "rb") as f:
+        char_stat_dict = pickle.load(f)
+
 
 
 
